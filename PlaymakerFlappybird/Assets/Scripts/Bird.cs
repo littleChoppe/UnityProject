0f697166f8d979896fb2fbf6b5aac61afbfe46ea
lu <1686143231@qq.com>
2016-11-16 23:14:04 +0800

    PlaymakerFlappybird

---
+++ PlaymakerFlappybird/Assets/Scripts/Bird.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bird : MonoBehaviour {
+
+    private Rigidbody2D rigidbody;
+    private PlayMakerFSM fsm;
+    private bool isGameOver = false;
+
+    void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        fsm = GetComponent<PlayMakerFSM>();
+    }
+
+    public void AddForce(Vector2 force)
+    {
+        Vector2 velocity = rigidbody.velocity;
+        velocity.y = 0;
+        rigidbody.velocity = velocity;
+        rigidbody.AddForce(force);
+    }
+
+    public void OnCollisionEnter2D(Collision2D col)
+    {
+        if (isGameOver)
+            return;
+        isGameOver = true;
+        fsm.SendEvent("GameOver");
+    }
+}
